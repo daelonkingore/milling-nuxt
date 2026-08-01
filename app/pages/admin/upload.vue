289d3b88dabc370de3790cd64e03ca13fc87e00f
daelonkingore <daelonkingore@gmail.com>
2026-08-01 10:38:32 -0500

    readd console logs

--- app/pages/admin/upload.vue
+++ app/pages/admin/upload.vue
@@ -74,6 +74,9 @@ async function delImage(image) {
 
   const token = user.value.token.access_token
 
+  console.log("image: " + image)
+  console.log("image.public_id: " + image?.public_id)
+
   await fetch('/.netlify/functions/deleteImage', {
     method: 'POST',
     headers: {
